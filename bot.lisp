eb6a07efdbaaefc6737de4ca0033c49b70ac0241
Edward John Steere <edward.steere@gmail.com>
2020-04-05 18:11:02 +0200

    Add function for extracting more sensible map

--- bot.lisp
+++ bot.lisp
@@ -3,7 +3,36 @@
 (defun main () 
   (loop while t
      for round-number = (read-line)
-     for state = (with-open-file (f (format nil "rounds/~a/state.json" round-number))
-                   (parse-state f))
+     for state = (load-state-file round-number)
      do (format t "C;~a;ACCELERATE~%" (current-round state))))
 
+(defun load-state-file (round)
+  "Load the state file for ROUND."
+  (load-state-from-file (format nil "rounds/~a/state.json" round)))
+
+(defun load-state-from-file (file-path)
+  "Load the state from file at FILE-PATH."
+  (with-open-file (f file-path)
+    (parse-state f)))
+
+(defun rows (state)
+  "Produce rows as a 2D array of cells from the map in STATE."
+  (iter
+    (with world-map = (slot-value state 'world-map))
+    (with result = (make-array (list (length world-map)
+                                     (length (aref world-map 0)))
+                               :initial-element nil))
+    (for y from 0)
+    (for row in-vector world-map)
+    (iter
+      (for cell in-vector row)
+      (for x from 0)
+      (setf (aref result y x)
+            (case (slot-value cell 'surface-object)
+              (0 nil)
+              (1 'mud)
+              (2 'oil-spill)
+              (3 'oil-item)
+              (4 'finish-line)
+              (5 'boost))))
+    (finally (return result))))
