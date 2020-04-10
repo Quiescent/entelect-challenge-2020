1341825af001dca6eceaebc1dd782f528c5bf738
Edward John Steere <edward.steere@gmail.com>
2020-04-10 10:49:51 +0200

    Add usage string to python runner script

--- run-against-versions.py
+++ run-against-versions.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python
 
-print("Ran!")
+import sys
+
+def main(argv):
+    """Run the version specified by the first argument against others."""
+    if len(argv) < 2:
+        usage()
+    print("Ran!")
+
+def usage():
+    """Print a usage help string."""
+    sys.stderr.writelines("""Usage:
+./run-against-versions <main-version> <other-version> [other-versions..]""")
+
+
+if __name__ == "__main__":
+    main(sys.argv)
