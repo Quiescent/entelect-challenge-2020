2db3a375105cb75ba2b1e73a63330c546585cde0
Edward John Steere <edward.steere@gmail.com>
2020-04-29 21:09:24 +0200

    Remove impossible moves

--- bot.lisp
+++ bot.lisp
@@ -122,7 +122,13 @@ Given that I'm at MY-POS, whether I'm BOOSTING, how many BOOSTS I have
 left, the SPEED at which I'm going and MY-ABS-X position on the
 board."
   (declare (ignore my-abs-x))
-  (car (sort (copy-seq all-moves)
+  (car (sort (remove-if (lambda (move) (or (not (move-can-be-made move
+                                                                  boosts
+                                                                  (cdr my-pos)))
+                                           (and (= 0 speed)
+                                                (or (eq move 'turn_right)
+                                                    (eq move 'turn_left)))))
+                        all-moves)
              #'>
              :key (lambda (move)
                     (move-score move
