f189f50dc08b4667a905ffb7ea15eafe2edc3f59
Edward John Steere <edward.steere@gmail.com>
2020-06-21 17:53:05 +0200

    Increase the depth of minimax

--- bot.lisp
+++ bot.lisp
@@ -161,7 +161,7 @@ Given that the player has BOOSTS, LIZARDS and TRUCKS left and is at
 POS."
   (remove-if (cannot-make-move boosts lizards trucks pos) all-makeable-moves))
 
-(defconstant maximax-depth 3
+(defconstant maximax-depth 4
   "The depth that we should search the game tree.")
 
 (defun make-opposed-move (game-map my-pos boosts
