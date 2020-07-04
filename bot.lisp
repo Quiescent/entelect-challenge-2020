d305f81d20b7adee169e3091462c4ff378726ff7
Edward John Steere <edward.steere@gmail.com>
2020-07-04 15:33:29 +0200

    Remove the weird boost sort

--- bot.lisp
+++ bot.lisp
@@ -336,11 +336,7 @@ board."
       copy-seq
       (stable-sort #'> :key (lambda (state) (if (eq (-> state car last car) 'use_boost) 0 1)))
       (stable-sort #'> :key (lambda (state) (car (nth 1 state))))
-      (stable-sort #'> :key (lambda (state) (nth 2 state)))
-      (stable-sort #'> :key (lambda (state) (if (and (eq (-> state car last car) 'use_boost)
-                                                (not (> (- (nth 2 state) speed) 3)))
-                                           0
-                                           1))))))
+      (stable-sort #'> :key (lambda (state) (nth 2 state))))))
 
 (defun only-those-which-dont-slow (end-states)
   "Filter END-STATES to those which don't lose speed or lose least."
