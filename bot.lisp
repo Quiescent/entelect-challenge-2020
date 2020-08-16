59eebe581c209a5631027a75485514047de69c1d
Edward John Steere <edward.steere@gmail.com>
2020-08-16 09:16:53 +0200

    Fix full game map argument

--- bot.lisp
+++ bot.lisp
@@ -604,7 +604,9 @@ MY-ABS-X position on the board."
                                            (cdr (player position))
                                            (opponent absolute-x)
                                            (cdr (opponent position)))
-                     (bind (((_ _ my-move _ depth) (make-opposed-move ,game-state *full-game-map*)))
+                     (bind (((_ _ my-move _ depth) (make-opposed-move ,game-state
+                                                                      (cons *full-game-map*
+                                                                            (cdr ,(cadr (assoc 'game-map game-state)))))))
                        (if (= depth 0) (make-speed-move ,game-state) my-move)))
                     ((close-to-end (player absolute-x)) (make-finishing-move ,game-state))
                     (t (make-speed-move ,game-state)))))
